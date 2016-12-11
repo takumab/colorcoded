afae14650e04973d976e96b4422a8041f1a36fba
takumab <takumab@gmail.com>
2016-12-11 19:20:49 +0000

    Add my styles back in application.scss and add events

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
+ActiveRecord::Schema.define(version: 20161204220056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-ActiveRecord::Schema.define(version: 20161204220056) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -24,6 +22,5 @@ ActiveRecord::Schema.define(version: 20161204220056) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
   end
-
 
 end
